0963e666421068f1203843640f2d96610c51c88c
Åsa Johansson <asa.j.1989@gmail.com>
2025-03-10 15:46:18 +0100

    Added routing to home/admin in menu. Also added viewData for heading om both pages.

---
+++ Alpha_Mvc/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alpha_Mvc.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home";
+            ViewData["Header"] = "Projects";
+            return View();
+        }
+    }
+}
--- Alpha_Mvc/Program.cs
+++ Alpha_Mvc/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Admin}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 //var AllowedConnections = "_allowedConnections";
