2971c27dca033563febc1434b5e601d4b57d35ca
Åsa Johansson <asa.j.1989@gmail.com>
2025-03-10 18:10:34 +0100

    Renamed view login to signin

---
+++ Alpha_Mvc/Controllers/SigninController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alpha_Mvc.Controllers
+{
+    [Route("signin")]
+    public class SigninController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Sign In";
+            return View();
+        }
+    }
+}
