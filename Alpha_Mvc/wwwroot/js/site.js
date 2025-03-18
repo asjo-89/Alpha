2c1bed4d8b464ce2783626c61cbe6324af652416
Åsa Johansson <asa.j.1989@gmail.com>
2025-03-18 12:00:47 +0100

    Added javascript for modal addProject.

--- Alpha_Mvc/wwwroot/js/site.js
+++ Alpha_Mvc/wwwroot/js/site.js
@@ -1,1 +1,29 @@
 ﻿
+// Open modal
+document.addEventListener('DOMContentLoaded', () => {
+    const modalButtons = document.querySelectorAll('[data-modal="true"]');
+
+    modalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const target = button.getAttribute('data-target');
+            const modal = document.querySelector(target);
+
+            if (modal) {
+                modal.style.display = 'flex';
+            }
+        });
+    });
+
+    // Close modal
+    const closeButtons = document.querySelectorAll('[data-close="true"]');
+
+    closeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = button.closest('.modal');
+
+            if (modal) {
+                modal.style.display = 'none';
+            }
+        });
+    });
+});
