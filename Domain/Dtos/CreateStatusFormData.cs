6ef0fb85e4e0b3876d0f552695e96220cff43b93
Åsa Johansson <asa.j.1989@gmail.com>
2025-04-09 13:43:15 +0200

    Added Dtos in Domain

---
+++ Domain/Dtos/CreateStatusFormData.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Dtos;
+
+public class CreateStatusFormData
+{
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Status  name", Prompt = "Enter status name")]
+    [DataType(DataType.Text)]
+    public string StatusName { get; set; } = null!;
+}
---
+++ Domain/Dtos/SignInFormData.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Dtos;
+
+public class SignInFormData
+{
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Email Address", Prompt = "Enter email address")]
+    [DataType(DataType.EmailAddress)]
+    [RegularExpression(@"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}")]
+    public string Email { get; set; } = null!;
+
+
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Password", Prompt = "Enter password")]
+    [DataType(DataType.Password)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':""\\|,.<>\/?]).{8,}$")]
+    public string Password { get; set; } = null!;
+
+
+    [Range(typeof(bool), "true", "true")]
+    public bool IsPersistent { get; set; }
+}
