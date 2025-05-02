1c5f7e5c08034aedb032bc2ee10cea0acd71948d
Åsa Johansson <asa.j.1989@gmail.com>
2025-05-02 19:30:54 +0200

    added javascript to sort projects by status and changed som css for the visuals.

---
+++ Alpha_Mvc/Controllers/SortController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alpha_Mvc.Controllers;
+
+public class SortController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
